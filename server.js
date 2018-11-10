b514f8647641f591471e6245b45bb6561aefea5e
Trevor Sullivan <trevor@trevorsullivan.net>
2018-11-10 08:28:35 -0800

    Included HTML template and referenced Spectre.css

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 
 // reply to request with "Hello World!"
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.render('index.html');
 });
 
 //start a server on port 80 and log its start to our console
