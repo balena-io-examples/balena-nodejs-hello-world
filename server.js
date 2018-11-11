4ac4daa6687f850ecd1673ebbc15e7ea259bf5b7
Trevor Sullivan <trevor@trevorsullivan.net>
2018-11-11 10:00:37 -0800

    Adding updated Balena welcome page

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ var app = express();
 // Enable HTML template middleware
 app.engine('html', require('ejs').renderFile);
 
+// Enable static CSS styles
+app.use(express.static('styles'));
+
 // reply to request with "Hello World!"
 app.get('/', function (req, res) {
   res.render('index.html');
