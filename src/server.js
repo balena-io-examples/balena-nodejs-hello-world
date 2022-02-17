252fba45a7dd2b3c7b33f1c2928f05d2ae1fbb2a
Vipul Gupta (@vipulgupta2048) <vipul@balena.io>
2022-02-17 11:47:29 +0000

    minor: Upgrade Getting Started experience for node
    Signed-off-by: Vipul Gupta (@vipulgupta2048) <vipul@balena.io>

--- src/server.js
+++ src/server.js
@@ -4,13 +4,13 @@ const favicon = require('serve-favicon')
 const app = express()
 const PORT = 80
 
-app.use(favicon(join(__dirname, 'public', 'favicon.ico')))
-app.set('views', join(__dirname, 'views'))
+app.use(favicon(join(__dirname, '../views/public', 'favicon.ico')))
+app.set('views', join(__dirname, '../views'))
 app.set('view engine', 'ejs')
 
 // Enable the public directory for resource files
 app.use('/public', express.static(
-  join(__dirname, 'public')
+  join(__dirname, '../views/public')
 ))
 
 // reply to request with the hello world html file
