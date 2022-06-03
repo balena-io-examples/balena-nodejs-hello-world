0d54f3832caf8c9e3121583f8c2793c171ffee79
Flynn Joffray <nucleardreamer@gmail.com>
2022-06-03 14:07:40 -0700

    moving files back into views directory
    Signed-off-by: Flynn Joffray <flynn@balena.io>
    Change-type: patch

--- src/server.js
+++ src/server.js
@@ -4,18 +4,18 @@ const favicon = require('serve-favicon')
 const app = express()
 const PORT = 80
 
-app.use(favicon(join(__dirname, '..', 'public', 'favicon.ico')))
-app.set('views', join(__dirname, '..', 'public'))
+app.use(favicon(join(__dirname, '..', 'views', 'public', 'favicon.ico')))
+app.set('views', join(__dirname, '..', 'views', 'public'))
 app.set('view engine', 'html')
 
 // Enable the public directory for resource files
 app.use('/public', express.static(
-  join(__dirname, '..', 'public')
+  join(__dirname, '..', 'views', 'public')
 ))
 
 // reply to request with the hello world html file
 app.get('/', function (req, res) {
-  res.sendFile(join(__dirname, '..', 'public', 'index.html'))
+  res.sendFile(join(__dirname, '..', 'views', 'index.html'))
 })
 
 // start a server on port 80 and log its start to our console
